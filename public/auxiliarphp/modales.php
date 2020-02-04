f618c7bb9a13bb518ee138750643db406b09bd61
Rafa <rafaelangelsobrino@gmail.com>
2020-02-04 14:13:57 +0100

    DES18:Admin_eventos - RAUS
     Modificado: conexion.php,modales.php,modalesjs,admin_eventos.blade.php
     Añadido: geolocate.js,gmaps.js

--- public/auxiliarphp/modales.php
+++ public/auxiliarphp/modales.php
@@ -18,7 +18,7 @@
             </div>
             <form action="" method="POST" enctype="multipart/form-data">
                 <div class="modal-body">
-                     <div class="right">
+                    <div class="right">
                         <div class="form-group">
                             <label>Nombre:</label>
                             <input name="nomb" type="text" class="form-control" placeholder="Introduce el nombre del evento" required>
@@ -44,13 +44,13 @@
                             <input name="portada" type="file" class="form-control-file">
                         </div>
                     </div>
-                    
+
                     <div class="left">
-                        
+
                         <div class="map">
-                            
+
                         </div>
-                        
+
                     </div>
 
                 </div>
@@ -100,13 +100,13 @@
                             <input name="portada" type="file" class="form-control-file">
                         </div>
                     </div>
-                    
+
                     <div class="left">
-                        
+
                         <div class="map">
-                            
+
                         </div>
-                        
+
                     </div>
 
                 </div>
@@ -117,3 +117,75 @@
         </div>
     </div>
 </div>
+<!-- Ventana modal para subir documentación -->
+
+<section class="modal fade" id="modalSubirDocumento">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content bg-transparent">
+            <div class="modal-header bg-dark text-white">
+                <div class="modal-title">
+                    Subir documentación
+                </div>
+                <span class="btn btn-danger" data-dismiss="modal">X</span>
+            </div>
+            <form name="formulario" action="" method="POST">
+                <div class="modal-body bg-light">
+                    <div class="form-group">
+                        <input type="text" class="pl-2 border border-dark" id="nombreDocumento" name="nombreDocumento" placeholder="Nombre del documento" required>
+                    </div>
+                    <div class="form-group">
+                        <textarea cols="40" rows="10" class="pl-2 border border-dark" id="descDocumento" name="descDocumento" placeholder="Descripción de la documentación"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input class="btn p-0" id="subirAdjuntos" name="subirAdjuntos" type="file">
+                    </div>
+                    <label for="subirAdjuntos">
+                        <span>Seleccionar adjuntos</span>
+                    </label>
+                </div>
+                <div class="modal-footer bg-light">
+                    <div class="text-right">
+                        <input type="submit" class="btn border border-dark" id="subirDocumento" name="subirDocumento" value="Subir documentación">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+
+<!-- Ventana modal para editar documentación -->
+
+<section class="modal fade" id="modalEditarDocumento">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content bg-transparent">
+            <div class="modal-header bg-dark text-white">
+                <div class="modal-title">
+                    Editar documentación
+                </div>
+                <span class="btn btn-danger" data-dismiss="modal">X</span>
+            </div>
+            <form name="formulario" action="" method="POST">
+                <div class="modal-body bg-light">
+                    <div class="form-group">
+                        <input type="text" class="pl-2 border border-dark" id="nombreDocumento" name="nombreDocumento" placeholder="Nombre del documento" required>
+                    </div>
+                    <div class="form-group">
+                        <textarea cols="40" rows="10" class="pl-2 border border-dark" id="descDocumento" name="descDocumento" placeholder="Descripción de la documentación"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input class="btn p-0" id="subirAdjuntos" name="subirAdjuntos" type="file">
+                    </div>
+                    <label for="subirAdjuntos">
+                        <span>Seleccionar adjuntos</span>
+                    </label>
+                </div>
+                <div class="modal-footer bg-light">
+                    <div class="text-right">
+                        <input type="submit" class="btn border border-dark" id="editarDocumento" name="editarDocumento" value="Confirmar cambios">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+
