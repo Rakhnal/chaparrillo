4ccf135b7ef9d6b763022d56813165f645f20475
daw205 <daw205@daw205-HP-15-Notebook-PC>
2020-02-06 13:31:58 +0100

    DES15: pagina editar usuario - SSC
    Ventana editar usuario actualizada
    css editarusuario actualizado

--- resources/views/principal/Editar_usuario.blade.php
+++ resources/views/principal/Editar_usuario.blade.php
@@ -16,13 +16,13 @@ Inicio
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDwKmL1KMaYg3Hl6ggnEnCVgCCHhtsgvEU&libraries=drawing&callback=initMap&sensor=true"></script>
 
 <link href="css/editUsu/cropper.min.css" rel="stylesheet" type="text/css"/>
-<div class="float col-12 ml-3">
+<div class="col-12 ml-3">
     <div class="row col-12 form_base">
         <div class="col-12 text-center mt-2">
             <h2>Editar perfil</h2>
         </div>
-        <div class="row col-12">
-            <div class=" col-4">
+        <div class="row col-12"><!--cambiar-->
+            <div class=" col-4" >
                 <?php $seesion_user = "stint"; ?>
                 <div id="profile-result" class="mb-2 justify-content-center d-flex">
                     <?php if (file_exists('images/profile-pic/' . $seesion_user . '.jpg')): ?>
@@ -34,7 +34,7 @@ Inicio
                 <script type="text/javascript" src="{{ URL::asset('scripts/general/main.js') }}"></script>
                 <script type="text/javascript" src="{{ URL::asset('scripts/general/cropper.min.js') }}"></script>
                 <!--        boostrap model change profile pic-->
-                <div class="col-12">
+                <div class="col-12" id="ajusteimagen">
                     <div class="ajax-response" id="loading"></div>
                     <h4 class="m-t-5 m-b-5 ellipsis text-center">Ajustar imagen</h4>                    
                     <div class="profile-pic-wraper">
@@ -56,39 +56,53 @@ Inicio
                     <div class="row">
                         <div class="col-6">
                             <label class="form_control col-12">Email:</label>
-                            <input type="email" name="email" class=" w-100 text_black" value="" required="">
+                            <input type="email" name="email" class=" w-100 text_black" value="COMEME" required="" id="email">
                         </div>
                         <div class="col-6">
                             <label class="form_control col-12">Contraseña:</label>
-                            <button type="button" class="btn btn-light w-100" data-toggle="modal" data-target="#exampleModalCenter">
-                                Cambiar contraseña
-                            </button>
+                            <input type="button" class="btn btn-light w-100" data-toggle="modal" data-target="#exampleModalCenter" id="password" value="Cambiar contraseña" disabled="true" > 
                         </div>
-
                     </div>
                     <div class="row">
                         <div class="col-6">
                             <label class="form_control col-12">Nombre:</label>
-                            <input type="text" name="nombre" class="w-100 text_black" value="">
+                            <input type="text" name="nombre" class="w-100 text_black" value="LOS" id="nombre">
                         </div>
                         <div class="col-6">
                             <label class="form_control col-12">Apellidos:</label>
-                            <input type="text" name="apellidos" class=" w-100 text_black" value="">
+                            <input type="text" name="apellidos" class=" w-100 text_black" value="" id="apell">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-12"><label class="form_control">Localización:</label></div>
                         <div class="col-6">
-                            <input name="Pais"  type="text" class="text_black w-100" placeholder="Pais">
+                            <input name="Pais"  type="text" class="text_black w-100" placeholder="Pais" value="HUEVOS" id="pais">
                         </div>
                         <div class="col-6">
-                            <input name="localidad" type="text" class="text_black w-100" placeholder="Localidad">
+                            <input name="localidad" type="text" class="text_black w-100" placeholder="Localidad" id="local">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-12">
                             <label class="form_control">Posición:</label>
                             <script>
+                                function blok() {
+                                    document.getElementById("email").disabled = true;
+                                    document.getElementById("pais").disabled = true;
+                                    document.getElementById("local").disabled = true;
+                                    document.getElementById("nombre").disabled = true;
+                                    document.getElementById("apell").disabled = true;
+                                    document.getElementById("password").disabled = true;   
+                                    document.getElementById("ajusteimagen").disabled = true;  
+                                }
+                                function desblok() {
+                                    document.getElementById("email").disabled = false;
+                                    document.getElementById("pais").disabled = false;
+                                    document.getElementById("local").disabled = false;
+                                    document.getElementById("nombre").disabled = false;
+                                    document.getElementById("apell").disabled = false;
+                                    document.getElementById("password").disabled = false; 
+                                }
                                 function initMap() {
                                     // Creamos un objeto mapa y especificamos el elemento DOM donde se va a mostrar.
                                     var map = new google.maps.Map(document.getElementById('mapa'), {
@@ -116,12 +130,12 @@ Inicio
                                 }
                             </script>
                             <div id="mapa" class="col-12"> </div>
-                            <script>initMap();</script>
+                            <script>initMap(); blok();</script>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-12 text-center mb-3">
-                    <button type="submit" class="btn btn-primary">Editar</button>
+                    <button type="button" class="btn btn-primary" onclick="desblok()">Editar</button>
                 </div>
             </form>
         </div>
