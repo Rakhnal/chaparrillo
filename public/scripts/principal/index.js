be8b5a999efc039f48359671746b0342794cd8b0
Álvaro <alvdc1997@gmail.com>
2020-02-06 13:31:03 +0100

    DES03: Página Index - ADC
    Cambiado los diseños de las pantallas de administración.
    
    Solucionado un error en la ventana de perfil.

--- public/scripts/principal/index.js
+++ public/scripts/principal/index.js
@@ -7,16 +7,16 @@
 $(document).mousemove(function (event) {
     if (event.pageX > (window.screen.availWidth / 2)) {
         document.body.style.backgroundColor = "#8F7E4F";
-        $('.dropdown-menu').css('background-color', 'rgba(92, 79, 43, 0.9)');
-        $('.navbar').css('background-color', 'rgba(92, 79, 43, 0.9)');
-        $('.modal-content').css('background-color', 'rgba(92, 79, 43, 0.9)');
-        $('.footer').css('background-color', 'rgba(92, 79, 43, 0.9)');
+        $('.dropdown-menu').css('background-color', 'rgba(92, 79, 43, 0.98)');
+        $('.navbar').css('background-color', 'rgba(92, 79, 43, 0.98)');
+        $('.modal-content').css('background-color', 'rgba(92, 79, 43, 0.98)');
+        $('.footer').css('background-color', 'rgba(92, 79, 43, 0.98)');
     } else {
         document.body.style.backgroundColor = "#78aa57";
-        $('.dropdown-menu').css('background-color', 'rgba(82, 117, 59, 0.9)');
-        $('.navbar').css('background-color', 'rgba(82, 117, 59, 0.9)');
-        $('.modal-content').css('background-color', 'rgba(82, 117, 59, 0.9)');
-        $('.footer').css('background-color', 'rgba(82, 117, 59, 0.9)');
+        $('.dropdown-menu').css('background-color', 'rgba(82, 117, 59, 0.98)');
+        $('.navbar').css('background-color', 'rgba(82, 117, 59, 0.98)');
+        $('.modal-content').css('background-color', 'rgba(82, 117, 59, 0.98)');
+        $('.footer').css('background-color', 'rgba(82, 117, 59, 0.98)');
     }
 });
 
