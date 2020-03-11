9929e614b556f2fd9cbae5d29607cab0df861b1e
daw205 <daw205@daw205-HP-15-Notebook-PC>
2020-03-11 12:31:40 +0100

    DES11 SSC ventana ver noticias modificada

--- app/Http/Controllers/controlador_noticias.php
+++ app/Http/Controllers/controlador_noticias.php
@@ -22,7 +22,7 @@ class controlador_noticias extends Controller {
                 ->join('usuarios','usuarios.id_user','=','publicaciones.id_user')
                 ->join('categorias','noticias.tipo','=','categorias.id_categoria')
                 ->join('imagenes','imagenes.id_item','=','publicaciones.id_item')
-                ->select('noticias.id_noticia','imagenes.imagen','usuarios.nombre as usuario','usuarios.apellidos','usuarios.email', 'categorias.nombre as categoria', 'noticias.enlace','publicaciones.descripcion','publicaciones.nombre as titulo','publicaciones.fecha_subida')
+                ->select('noticias.id_noticia','noticias.destacada','imagenes.imagen','usuarios.nombre as usuario','usuarios.apellidos','usuarios.email', 'categorias.nombre as categoria', 'noticias.enlace','publicaciones.descripcion','publicaciones.nombre as titulo','publicaciones.fecha_subida')
                 ->orderby('publicaciones.fecha_subida','desc')
                 ->paginate(8);
         $destacadas = DB::table('noticias')
@@ -30,6 +30,7 @@ class controlador_noticias extends Controller {
                 ->join('categorias','noticias.tipo','=','categorias.id_categoria')
                 ->select('noticias.id_noticia','categorias.nombre as categoria','publicaciones.nombre as titulo','publicaciones.fecha_subida')
                 ->orderby('publicaciones.fecha_subida','desc')
+                ->where('noticias.destacada','=','1')
                 ->paginate(10);
         return view(Constantes::AD_NOTICIAS, ['noticias' => $noticias,'destacadas'=>$destacadas]);
     }
--- resources/views/principal/Noticias.blade.php
+++ resources/views/principal/Noticias.blade.php
@@ -4,6 +4,8 @@ use App\Clases\conexion;
 use App\Clases\Auxiliares\Constantes;
 
 session()->put("actPage", Constantes::AD_NOTICIAS);
+$user = session()->get("userObj");
+$rol = $user->rol;
 ?>
 @extends('../general/base')
 @section('titulo')
@@ -14,17 +16,29 @@ Inicio
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <link href="css/noticias/noticias.css" rel="stylesheet" type="text/css"/>
 <div class="mr-1 ml-1 mt-1 row">
+    <div class="contenedor">
+        <button class="botonF1">
+            <span>+</span>
+        </button>
+    </div>
     <div class="col-8 mt-1 ">
         <?php foreach ($noticias as $noticia) { ?>
             @csrf<?php ?>
-            <div class="mt-3 mb-3 form_base noticia_base col-12 pr-0">
+            <div class="mt-3 mb-3 form_base noticia_base col-12 pr-0 titulo_noticia">
                 <div class="col-8">
-                    <div class="encabezado_noticia row">
-                        <a class="categoria_noticia col-8"><span class="block"><?= $noticia->categoria; ?></span></a>
-                        <a class="fecha_noticias col-4"><?= $noticia->fecha_subida; ?></a>
+                    <div class="row">
+                        <div class="col-1">
+                            <?php
+                            if ($rol > 0) {
+                                ?><input class="btn btn-no-destacada" id="destacar" type="submit" name="destacar" value=""><?php
+                            }
+                            ?>
+                        </div>
+                        <div class="col-8 text-center"><a><span class="block"><?= $noticia->categoria; ?></span></a></div>
+                        <div class="col-3"><a><?= $noticia->fecha_subida; ?></a></div>
                     </div>
-                    <div class="cabecera_noticia mt-2">
-                        <h2 class="titulo_noticia">
+                    <div class="mt-2">
+                        <h2>
                             <a href=""><?= $noticia->titulo; ?></a>
                         </h2>  
                     </div>
@@ -43,16 +57,17 @@ Inicio
         <?php } ?>
     </div>
     <div class="col-4 mt-1">
+        <div class="mt-3 mb-3 row destacadas text-center titulo_noticia"><h1><img src="images/icons/king.png" alt="Imagen" class="icon">   Destacadas</h1></div>
         <?php foreach ($destacadas as $destacada) { ?>
-            <div class="mt-3 mb-3 form_base">
-                <div class="encabezado_noticia">
-                    <a class="fecha_noticias col-4"><?= $destacada->fecha_subida; ?></a>
-                    <a class="categoria_noticia col-4"><span class="block"><?= $destacada->categoria; ?></span></a>
+            <div class="mt-3 mb-3 titulo_noticia row destacadas">
+                <div class="row">
+                    <div class="col-5 ml-2"><a><?= $destacada->fecha_subida; ?></a></div>
+                    <div class="col-5"><a><span class="block"><?= $destacada->categoria; ?></span></a></div>
                 </div>
-                <div class="cabecera_noticia">
-                    <h2 class="titulo_noticia">
+                <div>
+                    <h4 class="ml-4">
                         <a href=""><?= $destacada->titulo; ?></a>
-                    </h2>  
+                    </h4>  
                 </div>
             </div>
         <?php } ?>
