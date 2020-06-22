4975f3fe70a9c205791f75b6fad4dbd722d521d6
winniecheese <nathaniellucasolmo@gmail.com>
2020-06-22 03:05:02 +0200

    Merge origin/master
    Conflicts:
    	resources/views/principal/Noticias.blade.php

--- app/Http/Controllers/usercontroller.php
+++ app/Http/Controllers/usercontroller.php
@@ -37,6 +37,9 @@ class usercontroller extends Controller {
         if (session()->get('actPage') == Constantes::DOCUMENTACION) {
             return redirect('documentacion');
         }
+        if (session()->get('actPage') == Constantes::AD_NOTICIAS) {
+            return redirect('noticias');
+        }
         if (session()->get('actPage') == Constantes::FORO || session()->get('actPage') == Constantes::VERFORO) {
             return redirect('foro');
         } else {
